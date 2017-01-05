db0fc7ffb58cb0b6c0d325c55355bafc0775d94e
Chris Cates <codeviolet@chriscates.ca>
2017-01-05 06:47:42 -0500

    Added config strategy

---
+++ ApiPlusOne/config.js
@@ -0,0 +1,19 @@
+module.exports = (ENV) => {
+  if (ENV == "development") {
+    return {
+        'ENV': ENV,
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'DATABASE': 'plusone'
+    }
+  } else {
+    return {
+        'ENV': ENV,
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'DATABASE': 'plusone'
+    }
+  }
+}
--- ApiPlusOne/server.js
+++ ApiPlusOne/server.js
@@ -1,13 +1,36 @@
+const ENV = process.env.NODE_ENV || "development";
+const PORT = process.env.PORT || 3000;
+
+const config = require('config.js')(ENV);
+
 const express = require('express');
 const app = express();
+
+const mysql = require('mysql');
+
+if (ENV == "development") {
+	const sqlite3 = require('sqlite3').verbose();
+	const connection = new sqlite3.Database('plusone.db');
+} else {
+	const connection = mysql.createConnection({
+		'host': config.HOST,
+		'user': config.USER,
+		'password': config.PASSWORD,
+		'database': config.DATABASE,
+		'multipleStatements': true
+	});
+}
+
+const nodeSql = require('nodesql');
+const db = nodeSql.createMySqlStrategy(connection);
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true });
 
-const ENV = process.env.NODE_ENV || "development";
-const PORT = process.env.PORT || 3000;
+app.set('router', express.Router());
+app.set('db', db);
 
 app.listen(PORT, () => {
 	console.log("API Plus One listening on PORT", PORT);

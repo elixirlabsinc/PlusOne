112a994285a1fbc966d0c19fa05aae1efefb532d
Chris Cates <codeviolet@chriscates.ca>
2017-01-30 22:58:21 -0500

    Initial mockups

--- WebPlusOne/src/app/app.module.ts
+++ WebPlusOne/src/app/app.module.ts
@@ -6,11 +6,25 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OnboardingComponent } from './onboarding/onboarding.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ChatComponent } from './chat/chat.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    OnboardingComponent,
+    MainComponent,
+    FooterComponent,
+    ChatComponent,
+    ProfileComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +34,30 @@ import { HomeComponent } from './home/home.component';
       {
         path: '',
         component: HomeComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'onboarding',
+        component: OnboardingComponent
+      },
+      {
+        path: 'home',
+        component: MainComponent
+      },
+      {
+        path: 'chat',
+        component: ChatComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'message',
+        component: MessageComponent
       }
     ])
   ],
